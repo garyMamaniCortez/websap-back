b008d8eca9b8935c619c230e765b196c754bde80
garyMamaniCortez <mamanijhonny2210@gmail.com>
2023-05-06 11:22:18 -0400

    update the endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\UsuarioController;
+use App\Http\Controllers\API\ReservasController;
+use App\Http\Controllers\API\SitioController;
+use App\Http\Controllers\API\HorarioGuardiaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/usuario/{id}', [UsuarioController::class,'show']);
+Route::post('/usuario/', [UsuarioController::class,'store']);
+Route::post('/login', [UsuarioController::class, 'login']);
+
+Route::get('/reservas', [ReservasController::class, 'index']);
+Route::get('/reservas/{id}', [ReservasController::class, 'show']);
+Route::post('/reserva', [ReservasController::class, 'store']);
+Route::put('/reserva/{id}', [ReservasController::class, 'update']);
+
+Route::put('/sitio/{id}', [SitioController::class, 'update']);
+
+Route::get('/horario/guardia', [HorarioGuardiaController::class, 'index']);
+//Route::get('/horario/guardia/{id}', [HorarioGuardiaController::class, 'show']);
+Route::post('/horario/guardia', [HorarioGuardiaController::class, 'store']);
+//Route::put('/horario/guardia/{id}', [HorarioGuardiaController::class, 'update']);
