6ca08b4ae37492864e8960bdd0bc7518a060cc5a
garyMamaniCortez <mamanijhonny2210@gmail.com>
2023-05-06 11:22:07 -0400

    populate the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('usuarios')->insert([
+            [
+                'nombres' => 'Gary',
+                'apellidos' => 'Mamani',
+                'email'=> 'gary.mamani@gmail.com',
+                'ci' => 8852586,
+                'celular' => 65385951,
+                'contraseña' => '12345678',
+                'tipo_usuario' => 'admin'
+            ],
+            [
+                'nombres' => 'Luis',
+                'apellidos' => 'Rosales',
+                'email'=> 'rosales.luis@gmail.com',
+                'ci' => 8852587,
+                'celular' => 65385952,
+                'contraseña' => '12345678',
+                'tipo_usuario' => 'guardia'
+            ]
+        ]);
+        DB::table('sitios')->insert([
+            [
+                'sitio' => 'A21',
+                'ocupado' => 1
+            ],
+            [
+                'sitio' => 'A22',
+                'ocupado' => 0
+            ]
+        ]);
+        DB::table('reservas')->insert([
+            [
+                'nombre_cliente' => 'Joselito',
+                'fecha_ini' => now(),
+                'codigo_sis' => 201900402,
+                'email' => 'gary.mamani@gmail.com',
+                'matricula' => '1234ABC',
+                'celular' => 65385951,
+                'sitio' => 1
+            ]
+        ]);
     }
 }
